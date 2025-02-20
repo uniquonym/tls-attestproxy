423c6d245ff86be73e4ec0ec0cfdc6f77106533c
Andrew Miller <andrew@amxl.com>
2025-02-20 22:29:32 +1100

    Refactor Attestation Key (AK) generation code.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use std::fs::read;
+use std::fs::{read, read_to_string};
 
 use actix_web::{get, rt, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_ws::AggregatedMessage;
 use futures_util::StreamExt as _;
+use serde::{Deserialize, Serialize};
 use tss_esapi::{
     attributes::ObjectAttributes,
     constants::SessionType,
-    handles::AuthHandle,
+    handles::KeyHandle,
     interface_types::{
         algorithm::{HashingAlgorithm, PublicAlgorithm},
         key_bits::RsaKeyBits,
         reserved_handles::Hierarchy,
-        session_handles::{AuthSession, PolicySession},
+        session_handles::AuthSession,
     },
     structures::{
-        Digest, Nonce, Public, PublicBuilder, PublicKeyRsa, PublicRsaParameters, RsaExponent,
-        RsaScheme, SymmetricDefinition, SymmetricDefinitionObject,
+        CreatePrimaryKeyResult, HashScheme, Public, PublicBuilder, PublicKeyRsa,
+        PublicRsaParameters, RsaExponent, RsaScheme, SymmetricDefinition,
+        SymmetricDefinitionObject,
     },
+    tss2_esys::ESYS_TR,
+    utils::PublicKey,
     Context, Tcti,
 };
 
@@ -52,6 +56,97 @@ async fn tlscertify(req: HttpRequest, stream: web::Payload) -> Result<HttpRespon
     Ok(res)
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct StorableKey {
+    handle: ESYS_TR,
+    public_key: PublicKey,
+}
+
+impl TryFrom<CreatePrimaryKeyResult> for StorableKey {
+    type Error = anyhow::Error;
+
+    fn try_from(value: CreatePrimaryKeyResult) -> Result<Self, Self::Error> {
+        Ok(Self {
+            handle: value.key_handle.into(),
+            public_key: value.out_public.try_into()?,
+        })
+    }
+}
+
+pub struct TpmResidentKey {
+    handle: KeyHandle,
+    public: PublicKey,
+}
+
+impl From<StorableKey> for TpmResidentKey {
+    fn from(value: StorableKey) -> Self {
+        TpmResidentKey {
+            handle: value.handle.into(),
+            public: value.public_key,
+        }
+    }
+}
+
+fn try_load_key(path: &str) -> anyhow::Result<TpmResidentKey> {
+    let data = read_to_string(path)?;
+    let storable_key: StorableKey = serde_json::from_str(&data)?;
+    Ok(storable_key.into())
+}
+
+fn save_key(key: &StorableKey, path: &str) -> anyhow::Result<()> {
+    std::fs::write(path, &serde_json::to_string(key)?)?;
+    Ok(())
+}
+
+fn load_or_create_ak(context: &mut Context) -> anyhow::Result<TpmResidentKey> {
+    let keypath = std::env::var("AK_STORAGE_PATH").expect("AK_STORAGE_PATH to be set");
+    if let Ok(key) = try_load_key(&keypath) {
+        return Ok(key);
+    }
+
+    let primary_public_template: Public = PublicBuilder::new()
+        .with_name_hashing_algorithm(HashingAlgorithm::Sha256)
+        .with_public_algorithm(PublicAlgorithm::Rsa)
+        .with_object_attributes(
+            ObjectAttributes::builder()
+                .with_fixed_tpm(true)
+                .with_fixed_parent(true)
+                .with_sensitive_data_origin(true)
+                .with_user_with_auth(true)
+                .with_restricted(true)
+                .with_sign_encrypt(true)
+                .build()
+                .unwrap(),
+        )
+        .with_rsa_parameters(
+            PublicRsaParameters::builder()
+                .with_symmetric(SymmetricDefinitionObject::Null)
+                .with_scheme(RsaScheme::RsaSsa(HashScheme::new(HashingAlgorithm::Sha256)))
+                .with_key_bits(RsaKeyBits::Rsa2048)
+                .with_exponent(RsaExponent::create(65537).unwrap())
+                .build()
+                .unwrap(),
+        )
+        .with_rsa_unique_identifier(PublicKeyRsa::default())
+        .build()
+        .expect("Expected to build public key template");
+
+    let primary = context
+        .create_primary(
+            Hierarchy::Endorsement,
+            primary_public_template,
+            None,
+            None,
+            None,
+            None,
+        )
+        .expect("Expected CreatePrimary to create EK to succeed");
+    let storable_primary: StorableKey = primary.try_into().unwrap();
+    save_key(&storable_primary, &keypath)?;
+
+    Ok(storable_primary.into())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_default_env().init();
@@ -72,72 +167,10 @@ async fn main() -> std::io::Result<()> {
         .expect("Expected to create an HMAC session.")
         .unwrap();
 
-    let trial_sess: PolicySession = context
-        .start_auth_session(
-            None,
-            None,
-            None,
-            SessionType::Trial,
-            SymmetricDefinition::AES_256_CFB,
-            HashingAlgorithm::Sha256,
-        )
-        .expect("Expected to create a trial session.")
-        .unwrap()
-        .try_into()
-        .unwrap();
     context.set_sessions((Some(hmac_sess), None, None));
-    // This needs to exactly match the TCG template (also used by Google).
-    context
-        .policy_secret(
-            trial_sess,
-            AuthHandle::Endorsement,
-            Nonce::default(),
-            Digest::default(),
-            Nonce::default(),
-            None,
-        )
-        .expect("Expected PolicySecret to succeed");
-    context.set_sessions((None, None, None));
-    let policy_digest = context.policy_get_digest(trial_sess).unwrap();
-    // println!("EK Policy Digest: {:02X?}", policy_digest.as_bytes());
+    let _ak = load_or_create_ak(&mut context).expect("Expected to have created attestation key");
 
-    let primary_public_template: Public = PublicBuilder::new()
-        .with_name_hashing_algorithm(HashingAlgorithm::Sha256)
-        .with_object_attributes(
-            ObjectAttributes::builder()
-                .with_fixed_tpm(true)
-                .with_fixed_parent(true)
-                .with_sensitive_data_origin(true)
-                .with_user_with_auth(true)
-                .with_restricted(true)
-                .with_sign_encrypt(true)
-                .build()
-                .unwrap(),
-        )
-        .with_auth_policy(policy_digest)
-        .with_rsa_parameters(
-            PublicRsaParameters::builder()
-                .with_symmetric(SymmetricDefinitionObject::AES_128_CFB)
-                .with_scheme(RsaScheme::Null)
-                .with_key_bits(RsaKeyBits::Rsa2048)
-                .with_exponent(RsaExponent::ZERO_EXPONENT)
-                .build()
-                .unwrap(),
-        )
-        .with_rsa_unique_identifier(PublicKeyRsa::new_empty_with_size(RsaKeyBits::Rsa2048))
-        .build()
-        .expect("Expected to build public key template");
-
-    context
-        .create_primary(
-            Hierarchy::Endorsement,
-            primary_public_template,
-            None,
-            None,
-            None,
-            None,
-        )
-        .expect("Expected CreatePrimary to create EK to succeed");
+    // context.create();
 
     HttpServer::new(|| App::new().service(binarylogsvc))
         .bind(("0.0.0.0", 8080))?
